64298f382de9af3702b6e98ef64e1cff0a80e731
David Castellanos <davcastellanoslarios@gmail.com>
2021-12-01 17:14:11 -0600

    cleaning the work space of react and creating two components

---
+++ src/components/Headers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Headers = () => {
+    return (
+        <header className='bg-alert' >
+            <h1> Search for your fav Drinks </h1>
+        </header>
+    )
+}
+
+export default Headers
